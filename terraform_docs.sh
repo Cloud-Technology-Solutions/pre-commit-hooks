e1e1a9136188453525bc247dea7cbce47d74b26a
Chris Cunningham <chris.cunningham@cloudsolutions.co.uk>
2021-02-15 16:30:48 +0100

    Update terraform_docs.sh

--- terraform_docs.sh
+++ terraform_docs.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-for d in $(ls -d */); do
-  terraform-docs md $d >$d/README.md
-  if [ $? -eq 0 ]; then
+for d in $(ls -d */ | egrep -v 'docs|site')
+do
+  terraform-docs md $d > $d/README.md
+  if [ $? -eq 0 ] ; then
     git add "./$d/README.md"
   fi
 done
