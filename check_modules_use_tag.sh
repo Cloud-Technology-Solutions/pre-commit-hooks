fba440321a788dba67de1612a60da985dd6c77ae
chriscunningham-cts <chris.cunningham@cloudsolutions.co.uk>
2021-03-17 12:18:37 +0000

    regex working;

--- check_modules_use_tag.sh
+++ check_modules_use_tag.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 for file in "$@"; do
-  if grep "^version:"; then
-    if ! grep "^version: v[0-9].[0-9].[0-9].$" "$file"; then
+  if grep -E '^version:' "$file"; then
+    if ! grep -E '^version: v([0-9]+)\.([0-9]+)\.([0-9]+)$' "$file"; then
       echo "file $file references a version which is not a tag, e.g. 'vX.X.X'"
       exit 1
     fi
