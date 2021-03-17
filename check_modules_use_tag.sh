c7d63703d83c86da97752191eff097cd8d0ac6c8
chriscunningham-cts <chris.cunningham@cloudsolutions.co.uk>
2021-03-17 11:15:15 +0000

    oops

--- check_modules_use_tag.sh
+++ check_modules_use_tag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for file in "$@"; do
-  if grep "^version: v[0-9].[0-9].[0-9].$" "$file"; then
+  if ! grep "^version: v[0-9].[0-9].[0-9].$" "$file"; then
     echo "file $file references a version which is not a tag, e.g. 'vX.X.X'"
     exit 1
   fi
