beffbcb93ca92fc744b5110f0a1b992e0c20d4a8
chriscunningham-cts <chris.cunningham@cloudsolutions.co.uk>
2021-03-17 11:14:07 +0000

    add error message

--- check_modules_use_tag.sh
+++ check_modules_use_tag.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 for file in "$@"; do
-  grep "^version: v[0-9].[0-9].[0-9].$" "$file"
+  if grep "^version: v[0-9].[0-9].[0-9].$" "$file"; then
+    echo "file $file references a version which is not a tag, e.g. 'vX.X.X'"
+    exit 1
+  fi
 done
