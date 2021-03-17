e0cd5012c058d9ec6fa3e6b849f38017103cf4fe
chriscunningham-cts <chris.cunningham@cloudsolutions.co.uk>
2021-03-17 11:11:13 +0000

    executable

--- terraform_docs.sh
+++ terraform_docs.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-for d in $(ls -d */ | egrep -v 'docs|site')
-do
-  terraform-docs md $d > $d/README.md
-  if [ $? -eq 0 ] ; then
+for d in $(find . -maxdepth 1 -type d -not -name '.*' | grep -Ev 'docs|site'); do
+  if terraform-docs md "$d" >"$d"/README.md; then
     git add "./$d/README.md"
   fi
 done
