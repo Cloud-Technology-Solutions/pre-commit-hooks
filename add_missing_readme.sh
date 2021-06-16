b03a907b8e095e52be637ee06bc76dc8cbd67d58
chriscunningham-cts <chris.cunningham@cloudsolutions.co.uk>
2021-06-16 17:12:49 +0200

    always fail

--- add_missing_readme.sh
+++ add_missing_readme.sh
@@ -3,7 +3,8 @@
 # adapted from https://github.com/r-lib/usethis/blob/master/inst/templates/readme-rmd-pre-commit.sh
 # only run if no readme means nothing to check
 if [[ -f "README.md" ]]; then
-  exit 0
+  echo 'found readme'
+  exit 1
 else
   echo 'lol no readme'
   exit 1
