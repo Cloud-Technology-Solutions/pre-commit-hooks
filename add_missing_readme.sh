13afe1852594caaeb7b46b741f944c1042d59918
chriscunningham-cts <chris.cunningham@cloudsolutions.co.uk>
2021-06-16 17:21:43 +0200

    last fake

--- add_missing_readme.sh
+++ add_missing_readme.sh
@@ -3,9 +3,12 @@
 # adapted from https://github.com/r-lib/usethis/blob/master/inst/templates/readme-rmd-pre-commit.sh
 # only run if no readme means nothing to check
 if [[ -f "README.md" ]]; then
-  echo "found readme at $(pwd)"
+  echo "$@"
+  pwd
   exit 0
 else
+  echo "$@"
+  pwd
   echo 'lol no readme'
   exit 1
 fi
