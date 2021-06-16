ce5e9ba5affcaf787c54a24a99130446e73da13c
chriscunningham-cts <chris.cunningham@cloudsolutions.co.uk>
2021-06-16 17:38:52 +0200

    should be working now

--- add_missing_readme.sh
+++ add_missing_readme.sh
@@ -2,13 +2,13 @@
 
 # adapted from https://github.com/r-lib/usethis/blob/master/inst/templates/readme-rmd-pre-commit.sh
 # only run if no readme means nothing to check
-if [[ -f "README.md" ]]; then
-  echo "$@"
-  pwd
-  exit 0
-else
-  echo "$@"
-  pwd
-  echo 'lol no readme'
-  exit 1
-fi
+
+for i; do
+  echo "Looking for README.md in $(dirname $i)"
+  if [[ -f "$(dirname $i)/README.md" ]]; then
+    echo "README.md found at $(dirname $i)/README.md"
+  else
+    echo "No README.md found in $(dirname $i)"
+    exit 1
+  fi
+done
