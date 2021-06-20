844aaa77730b7c363a82583734ed2c98f8a6e033
chriscunningham-cts <chris.cunningham@cloudsolutions.co.uk>
2021-06-20 17:05:06 +0000

    only error after checking all inputs

--- hooks/add-missing-readme.sh
+++ hooks/add-missing-readme.sh
@@ -3,6 +3,7 @@
 # adapted from https://github.com/r-lib/usethis/blob/master/inst/templates/readme-rmd-pre-commit.sh
 # only run if no readme means nothing to check
 
+errors=0
 for file_to_check; do
   file_dir=$(dirname "$file_to_check")
   # echo "Looking for README.md in $file_dir"
@@ -16,6 +17,9 @@ TODO
 <!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
 <!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
 EOF
-    exit 1
+    errors=1
   fi
 done
+if [[ errors != 0 ]]; then
+  exit 1
+fi
